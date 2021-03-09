eb4bed2d14cf87692b2a298f05f6e40e470ce3e4
Alexandr Klychnikov <klychnikov75@mail.ru>
2021-03-09 20:18:21 +0300

    feat: create function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let {
+   repeatTimes = 1, 
+   separator = '+', 
+   addition,
+   additionRepeatTimes = 1, 
+   additionSeparator = '|'} = options;
+
+   if (str === null) str = 'null';
+   if (typeof(str) == 'object' && !"Symbol.toPrimitive" in str) {
+    str = str[Symbol.toPrimitive]('string');
+   }   
+   if (typeof(str) !== 'string') str=String(str);
+
+   if (addition === null) addition = 'null';
+   if (typeof(addition) == 'object' && !"Symbol.toPrimitive" in addition) {
+    addition = addition[Symbol.toPrimitive]('string');
+   } 
+   if (typeof(addition) !== 'string') addition=String(addition);
+
+   let addStrGross = (addition+additionSeparator).repeat(additionRepeatTimes);
+   let extraSeparator = additionSeparator.length
+   if (repeatTimes == "undefined") {
+     addStrGross = addition;
+     extraSeparator = 0;
+   }
+   let addStrNet = addStrGross.slice(0, addStrGross.length - extraSeparator);
+   if (addStrNet == "undefined") addStrNet = '';
+   let newStr = (str.concat(addStrNet)).concat(separator).repeat(repeatTimes);
+   let ultimateStr = newStr.slice(0, newStr.length - separator.length);
+
+   return ultimateStr;
+
 };
-  
